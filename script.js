af2bb7738f028888fd63eef8ac8ce616feca3e2b
Juneid Teklu <te.juneid@gmail.com>
2024-06-01 15:18:53 -0500

    Begin creating dialog modal

---
+++ script.js
@@ -0,0 +1,5 @@
+let bookCardArea = document.querySelector(".library-main");
+let addBook = document.querySelector(".add-book")
+let dialog = document.querySelector("dialog")
+
+addBook.addEventListener("click", ()=> {dialog.showModal()})
