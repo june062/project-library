606a87c558ea4af4e646ef148bff8450fae65bf2
Juneid Teklu <te.juneid@gmail.com>
2024-06-07 13:50:36 -0500

    Add the option to toggle between Read and Not Read. Finish project

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function displayLastLibraryItem(){
     let bookCard = document.createElement("div");
     bookCard.classList.add("book-card");
     bookCard.setAttribute("data-bookId", myLibrary[myLibrary.length-1].bookId); 
+    bookCard.setAttribute("id",myLibrary[myLibrary.length-1].bookId )
     bookCardArea.appendChild(bookCard);
 
     let titleSpan =  document.createElement("span");
@@ -32,6 +33,10 @@ function displayLastLibraryItem(){
     let readSpan =  document.createElement("span");
     readSpan.classList.add("read");
 
+    let readToggle = document.createElement("input");
+    readToggle.setAttribute("type", "checkbox");
+    readToggle.setAttribute("id", "readToggle");
+
     let deleteCard = document.createElement("button");
     deleteCard.classList.add("delete-card");
     deleteCard.textContent = "Delete";
@@ -41,11 +46,13 @@ function displayLastLibraryItem(){
     bookCard.appendChild(authorSpan);
     bookCard.appendChild(pagesSpan);
     bookCard.appendChild(readSpan);
+    bookCard.appendChild(readToggle)
     bookCard.appendChild(deleteCard);
+   
 
-    authorSpan.textContent = myLibrary[myLibrary.length - 1].author;
-    titleSpan.textContent = myLibrary[myLibrary.length - 1].title;
-    pagesSpan.textContent = myLibrary[myLibrary.length - 1].pages;
+    authorSpan.textContent = `Author: ${myLibrary[myLibrary.length - 1].author}`;
+    titleSpan.textContent = `Title: ${myLibrary[myLibrary.length - 1].title}`;
+    pagesSpan.textContent = `${myLibrary[myLibrary.length - 1].pages} pages`;
     readSpan.textContent = myLibrary[myLibrary.length - 1].read;
 
 }
@@ -59,14 +66,24 @@ function Book(title, author, pages, read){
     this.read = read;
 }
 
-
-
+Book.prototype.readToggle = function (){
+    let readDOMEl = document.querySelector(`[data-bookid ='${this.bookId}'] > span.read`)
+   if (this.read === "Read"){
+        this.read = "Not Read";
+        readDOMEl.textContent = "Not Read";
+   }
+   else {
+        this.read = "Read"
+        readDOMEl.textContent = "Read";
+   }
+}
 
 function addBookToLibrary(){
     let titleVal = title.value;
     let authorVal = author.value;
     let pagesVal = pages.value;
-    let readVal = read.checked;
+    let readVal = read.checked? "Read" : "Not Read";
+    
 
     let newBook = new Book(titleVal, authorVal, pagesVal, readVal);
     myLibrary.push(newBook);
@@ -75,21 +92,35 @@ function addBookToLibrary(){
 
 }
 
+bookCardArea.addEventListener("click", (event)=>{
+    if (event.target.id != "readToggle"){
+        return;
+    }
+    else {
+        let domData = +event.target.parentNode.dataset.bookid;
+        let bookIndex = myLibrary.findIndex(element => {
+            return element.bookId === domData; });
+        
+        myLibrary[bookIndex].readToggle();
+    }
+
+})
 
 bookCardArea.addEventListener("click", (event)=>{
-   /*  let bookArrayString = event.target.parentNode.dataset.arrayindex;
-    let bookArrayNum = +bookArrayString;
-    myLibrary.splice(bookArrayNum, 1);  */
-    let domData = +event.target.parentNode.dataset.bookid;
-
-
-    let bookIndex = myLibrary.findIndex(element => {
-        return element.bookId === domData; 
-    })
-    myLibrary.splice(bookIndex,1);
-    if(event.target.textContent === "Delete"){
+    if(event.target.textContent != "Delete"){
+        return}
+    else{
         event.target.parentNode.remove();
-    }
+    
+        let domData = +event.target.parentNode.dataset.bookid;
+
+
+        let bookIndex = myLibrary.findIndex(element => {
+            return element.bookId === domData; 
+        })
+        myLibrary.splice(bookIndex,1);
+}
+    
 })
 
 
